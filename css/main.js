cab05584163cf70f80bc218a10d281d354257bb7
Vishal Thakur <157201590+vishalthakur0012@users.noreply.github.com>
2024-02-02 17:33:28 +0530

    Add files via upload

---
+++ css/main.js
@@ -0,0 +1,16 @@
+// SidebarModel js==================
+
+function toggleSidebar() {
+  var sidebarModal = document.getElementById("sidebarModal");
+  sidebarModal.classList.toggle("active");
+}
+
+document
+  .getElementById("burgerButton")
+  .addEventListener("click", toggleSidebar);
+
+
+
+
+// navbar 
+
